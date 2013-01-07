a62a8902649be576900c3cbb17c182bf12b22826
Lauren McCarthy <laurmccarthy@gmail.com>
2013-01-07 10:26:52 -0800

    added anal

--- header.php
+++ header.php
@@ -4,4 +4,5 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<title>howwas.us</title>
 	<link media="all" rel="stylesheet" type="text/css" href=<?php echo getRoot(); ?>/style.css />
+	<?php include('analytics.php'); ?>
 </head>
